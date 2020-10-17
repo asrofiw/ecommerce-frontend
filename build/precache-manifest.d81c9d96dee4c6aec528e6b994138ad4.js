bc2bad98cb003f61fd1136c58aba982c4163a559
asrofhy <asrofiw@users.noreply.github.com>
2020-10-18 03:43:23 +0700

    add build folder

---
+++ build/precache-manifest.d81c9d96dee4c6aec528e6b994138ad4.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaefee200e12d458b33df8b9f413c57b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1060c3c9e3dba5d33c16",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "975c11baa58dfbd52a67",
+    "url": "/static/css/main.e5f29c60.chunk.css"
+  },
+  {
+    "revision": "1060c3c9e3dba5d33c16",
+    "url": "/static/js/2.9435d381.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9435d381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975c11baa58dfbd52a67",
+    "url": "/static/js/main.0b35d93c.chunk.js"
+  },
+  {
+    "revision": "3cc9d6562f64fc197f20",
+    "url": "/static/js/runtime-main.fc30dc24.js"
+  },
+  {
+    "revision": "87754cec32e92fb2d38298e0c10456c3",
+    "url": "/static/media/bell.87754cec.svg"
+  },
+  {
+    "revision": "2a2dfc5f49134084908f146fb8e413a0",
+    "url": "/static/media/cart.2a2dfc5f.svg"
+  },
+  {
+    "revision": "debe99828040a4916033f55116e12a47",
+    "url": "/static/media/clipboard.debe9982.svg"
+  },
+  {
+    "revision": "39eb0bbf2cc33ba02f53f8585004f820",
+    "url": "/static/media/default-avatar.39eb0bbf.jpg"
+  },
+  {
+    "revision": "ddd963837304047300fd82030a57480c",
+    "url": "/static/media/edit.ddd96383.svg"
+  },
+  {
+    "revision": "a6c8d9b55c4050f5ac103e0b7c3583de",
+    "url": "/static/media/filter.a6c8d9b5.svg"
+  },
+  {
+    "revision": "f30487ea440656edd723db8dadf0dad4",
+    "url": "/static/media/jacket.f30487ea.png"
+  },
+  {
+    "revision": "26a0586e788a61f9ed3a46a9a2ca1bf5",
+    "url": "/static/media/logo-44px.26a0586e.svg"
+  },
+  {
+    "revision": "d503db415d56e335cc982ebb7c6adfd5",
+    "url": "/static/media/mail.d503db41.svg"
+  },
+  {
+    "revision": "395fccf9ecc123ba902d60c2806fb69d",
+    "url": "/static/media/map.395fccf9.svg"
+  },
+  {
+    "revision": "08441b705aab86a94ad6e4f1293d43c5",
+    "url": "/static/media/no_img.08441b70.png"
+  },
+  {
+    "revision": "c166939c83c62f67ace81531edb2c8ce",
+    "url": "/static/media/pants.c166939c.png"
+  },
+  {
+    "revision": "571c3ec6ee32d0fd8f38d5fad7a0ca7f",
+    "url": "/static/media/profile.571c3ec6.jpg"
+  },
+  {
+    "revision": "3a6a1e6549af60f9784e00e3be86f17c",
+    "url": "/static/media/search.3a6a1e65.svg"
+  },
+  {
+    "revision": "97e6e2050c0eab0e8c309bffd2e4c423",
+    "url": "/static/media/shoes.97e6e205.png"
+  },
+  {
+    "revision": "b0d025d89a68fb173292aebf1c702bc2",
+    "url": "/static/media/shorts.b0d025d8.png"
+  },
+  {
+    "revision": "e8493ce5e6c75dcbd8d94185da707ef0",
+    "url": "/static/media/slide-2.e8493ce5.jpg"
+  },
+  {
+    "revision": "bb53e403fede9a3e05b7d6ca1731c1c6",
+    "url": "/static/media/slide-3.bb53e403.jpg"
+  },
+  {
+    "revision": "cfe5695f2fc7a7e3a4b0177b242172a5",
+    "url": "/static/media/star-activated.cfe5695f.svg"
+  },
+  {
+    "revision": "74f7713064201ba412d6b0d923d0c82f",
+    "url": "/static/media/t-shirt.74f77130.png"
+  },
+  {
+    "revision": "861c99ec678a7428d46f1eea4d953e00",
+    "url": "/static/media/user.861c99ec.svg"
+  }
+]);
